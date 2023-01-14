f24cd3d2da3616620b05a6a2a3bb58762f69a345
sergioyanez <sergiomyanez02@gmail.com>
2023-01-14 16:57:40 -0300

    Se refactoriaza DB 4

--- database/seeders/EstadoHabilitacionSeeder.php
+++ database/seeders/EstadoHabilitacionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 use App\Models\Estado_habilitacion;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EstadoHabilitacionSeeder extends Seeder
 {
@@ -14,29 +15,29 @@ class EstadoHabilitacionSeeder extends Seeder
      */
     public function run()
     {
-        $estadohab1 = new Estado_habilitacion();
-        $estadohab1->descripcion = "Provisoria";
-    //    $estadohab1->fecha_vencimiento->date();
-        $estadohab1->save();
+        $data = [
+            [
+                'descripcion' => "Provisoria",
+                'fecha_vencimiento'=>now()->addDays(30)
+            ],
+            [
+                'descripcion' => "Permanente",
+                'fecha_vencimiento'=>now()->now()->addDays(90)
+            ],
+            [
+                'descripcion' => "Definitiva",
+                'fecha_vencimiento'=>null
+            ],
+            [
+                'descripcion' => "Especial",
+                'fecha_vencimiento'=>now()->addYear()
+            ],
+            [
+                'descripcion' => "Nocturna",
+                'fecha_vencimiento'=>now()->addYear()
+            ]
 
-        $estadohab2 = new Estado_habilitacion();
-        $estadohab2->descripcion = "Permanente";
-    //    $estadohab2->fecha_vencimiento->date();
-        $estadohab2->save();
-
-        $estadohab3 = new Estado_habilitacion();
-        $estadohab3->descripcion = "Definitiva";
-    //    $estadohab3->fecha_vencimiento->date();
-        $estadohab3->save();
-
-        $estadohab4 = new Estado_habilitacion();
-        $estadohab4->descripcion = "Nocturna";
-    //    $estadohab4->fecha_vencimiento->date();
-        $estadohab4->save();
-
-        $estadohab5 = new Estado_habilitacion();
-        $estadohab5->descripcion = "Especial";
-    //    $estadohab5->fecha_vencimiento->date();
-        $estadohab5->save();
+        ];
+      DB::table('estados_habilitacion')->insert($data);
     }
 }
