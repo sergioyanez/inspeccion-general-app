4b6c0d1a3f491f6e573b0aea4772bf589d90ce9a
sergioyanez <sergiomyanez02@gmail.com>
2023-01-26 13:35:04 -0300

    no se puede hacer cambio de rama local sin commitear

---
+++ app/Models/expediente_persona_juridica.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class expediente_persona_juridica extends Model
+{
+    use HasFactory;
+    protected $table = 'expedientes_personas_juridicas';
+}
