26e2d2fe0c84e5346813249d60ab6d49d9a4ce4a
sergioyanez <sergiomyanez02@gmail.com>
2023-01-29 12:36:26 -0300

    cambio 2

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TipoDniController;
-
+use App\Http\Controllers\TipoEstadoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,8 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('getDni', [TipoDniController::class, 'show'])->name('dni');
-Route::post('newDni', [TipoDniController::class, 'create']);
-Route::get('delete/{id}', [TipoDniController::class, 'destroy'])->name('delete');
-Route::get('edit/{id}', [TipoDniController::class, 'showOne'])->name('edit');
-Route::post('edit/{id}', [TipoDniController::class, 'update'])->name('editarNuevoDni');
+Route::controller(TipoDniController::class)->group(function(){
+    Route::get('getDni','show')->name('home');
+    Route::post('newDni','create');
+    Route::get('delete/{id}','destroy')->name('delete');
+    Route::get('edit/{id}','showOne')->name('edit');
+    Route::post('edit/{id}','update')->name('editarNuevoDni');
+});
+
+Route::controller(TipoEstadoController::class)->group(function(){
+    Route::get('getEstadoHabilitacion','show')->name('home');
+    Route::post('newEstadoHabilitacion','create');
+    Route::get('deleteEstadoHabilitacion/{id}','destroy')->name('delete');
+    Route::get('editEstadoHabilitacion/{id}','showOne')->name('edit');
+    Route::post('editEstadoHabilitacion/{id}','update')->name('editarEstadoHabilitacion');
+});
