4d908cfdf7e4884dbeeb229c3930cac1b3bd5344
seba <sebaesains77@gmail.com>
2023-02-01 10:21:37 -0300

    log y controlador de contribuyente

--- app/Http/Controllers/LogsContribuyenteController.php
+++ app/Http/Controllers/LogsContribuyenteController.php
@@ -35,9 +35,31 @@ class LogsContribuyenteController extends Controller
      * @param  \App\Http\Requests\Storelogs_contribuyenteRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Storelogs_contribuyenteRequest $request)
+    public function store($contribuyente, $char)
     {
-        //
+        $logs_contribuyente = new logs_contribuyente();
+        $user = auth()->user();
+
+        $logs_contribuyente->contribuyente_id = $contribuyente->id;
+        $logs_contribuyente->tipo_dni_id = $contribuyente->tipo_dni_id;
+        $logs_contribuyente->estado_civil_id = $contribuyente->estado_civil_id;
+        $logs_contribuyente->cuit = $contribuyente->cuit;
+        $logs_contribuyente->ingresos_brutos = $contribuyente->ingresos_brutos;
+        $logs_contribuyente->nombre = $contribuyente->nombre;
+        $logs_contribuyente->apellido = $contribuyente->apellido;
+        $logs_contribuyente->dni = $contribuyente->dni;
+        $logs_contribuyente->fecha_nacimiento = $contribuyente->fecha_nacimiento;
+        $logs_contribuyente->telefono = $contribuyente->telefono;
+        $logs_contribuyente->nombre_conyuge = $contribuyente->nombre_conyuge;
+        $logs_contribuyente->apellido_conyuge = $contribuyente->apellido_conyuge;
+        $logs_contribuyente->dni_conyuge = $contribuyente->dni_conyuge;
+        $logs_contribuyente->accion = $char;
+        //$logs_contribuyente->usuario_id = $user->id; -> PORBAR CON USUARIO
+        //$logs_tipo_dni->usuario_nombre = $user->usuario; -> IDEM ANTERIOR
+
+        $logs_contribuyente->save();
+        
+        return 'guardado';
     }
 
     /**
@@ -69,9 +91,31 @@ class LogsContribuyenteController extends Controller
      * @param  \App\Models\logs_contribuyente  $logs_contribuyente
      * @return \Illuminate\Http\Response
      */
-    public function update(Updatelogs_contribuyenteRequest $request, logs_contribuyente $logs_contribuyente)
+    public function update($contribuyente, $char)
     {
-        //
+        $logs_contribuyente = new logs_contribuyente();
+        $user = auth()->user();
+
+        $logs_contribuyente->contribuyente_id = $contribuyente->id;
+        $logs_contribuyente->tipo_dni_id = $contribuyente->tipo_dni_id;
+        $logs_contribuyente->estado_civil_id = $contribuyente->estado_civil_id;
+        $logs_contribuyente->cuit = $contribuyente->cuit;
+        $logs_contribuyente->ingresos_brutos = $contribuyente->ingresos_brutos;
+        $logs_contribuyente->nombre = $contribuyente->nombre;
+        $logs_contribuyente->apellido = $contribuyente->apellido;
+        $logs_contribuyente->dni = $contribuyente->dni;
+        $logs_contribuyente->fecha_nacimiento = $contribuyente->fecha_nacimiento;
+        $logs_contribuyente->telefono = $contribuyente->telefono;
+        $logs_contribuyente->nombre_conyuge = $contribuyente->nombre_conyuge;
+        $logs_contribuyente->apellido_conyuge = $contribuyente->apellido_conyuge;
+        $logs_contribuyente->dni_conyuge = $contribuyente->dni_conyuge;
+        $logs_contribuyente->accion = $char;
+        //$logs_contribuyente->usuario_id = $user->id; -> PORBAR CON USUARIO
+        //$logs_tipo_dni->usuario_nombre = $user->usuario; -> IDEM ANTERIOR
+
+        $logs_contribuyente->save();
+        
+        return 'actualizado';
     }
 
     /**
