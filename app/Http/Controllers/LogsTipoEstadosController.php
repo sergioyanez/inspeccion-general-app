5bee9935595a7f4c1b08eb09b63e8cf4bc43a6d5
sergioyanez <sergiomyanez02@gmail.com>
2023-02-01 16:40:25 -0300

    Se corrige código en LogsTipoEstadosController

--- app/Http/Controllers/LogsTipoEstadosController.php
+++ app/Http/Controllers/LogsTipoEstadosController.php
@@ -10,92 +10,23 @@ use App\Http\Requests\Updatelogs_tipo_estadosRequest;
 class LogsTipoEstadosController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Crea un nuevo registro de log Tipo_estado
+     *@param tipo_estado
+     *@param char
      */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create($tipo_estado, $char) {
+    public function store($tipo_estado, $char) {
 
         $logs_tipo_estado = new logs_tipo_estados();
-        //$user = auth()->user();
+        $user = auth()->user();
 
         $logs_tipo_estado->tipo_estado_id = $tipo_estado->id;
         $logs_tipo_estado->descripcion = $tipo_estado->descripcion;
         $logs_tipo_estado->accion = $char;
-        //$logs_tipo_dni->fecha_creacion = date();  -> NO VA
-        //$logs_tipo_dni->fecha_modificacion = date(); -> NO VA
-        //$logs_tipo_dni->usuario_id = $user->id; -> PROBAR CON USUARIO
-        //$logs_tipo_dni->usuario_nombre = $user->usuario; -> IDEM ANTERIOR
+        //$logs_tipo_estado->usuario_id = $user->id;
+        //$logs_tipo_estado->usuario_nombre = $user->usuario;
 
         $logs_tipo_estado->save();
 
-
-        return 'log estado guardado';
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\Storelogs_tipo_estadosRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Storelogs_tipo_estadosRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\logs_tipo_estados  $logs_tipo_estados
-     * @return \Illuminate\Http\Response
-     */
-    public function show(logs_tipo_estados $logs_tipo_estados)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\logs_tipo_estados  $logs_tipo_estados
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(logs_tipo_estados $logs_tipo_estados)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\Updatelogs_tipo_estadosRequest  $request
-     * @param  \App\Models\logs_tipo_estados  $logs_tipo_estados
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Updatelogs_tipo_estadosRequest $request, logs_tipo_estados $logs_tipo_estados)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\logs_tipo_estados  $logs_tipo_estados
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(logs_tipo_estados $logs_tipo_estados)
-    {
-        //
+        //return 'log estado guardado';
     }
 }
