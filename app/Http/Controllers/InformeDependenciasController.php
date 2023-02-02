f172b60dee544b78fb12b22c482875e51615b125
Euge Miller <eugeniomiller93@gmail.com>
2023-02-02 11:40:34 -0300

    Se crea funcionalidad en InformeDependenciaController y su respectivo log

--- app/Http/Controllers/InformeDependenciasController.php
+++ app/Http/Controllers/InformeDependenciasController.php
@@ -6,82 +6,120 @@ use App\Models\informe_dependencias;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Storeinforme_dependenciasRequest;
 use App\Http\Requests\Updateinforme_dependenciasRequest;
+use App\Http\Controllers\LogsInformeDependenciaController;
+use Illuminate\Http\Response;
 
-class InformeDependenciasController extends Controller
-{
+class InformeDependenciasController extends Controller {
+    
     /**
-     * Display a listing of the resource.
+     * Método que retorna todos los informes de dependencia
+     * existentes en  la base de datos
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    public function index() {
+        $informe_dependencias = informe_dependencia::all();
+        return view('informeDependencia.informeDependencia', ['informes_dependencias', $informe_dependencias]);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Método que me lleva a un formulario para
+     * agregar un nuevo informe de dependencia
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create() {
+        return view('informeDependencia.crear');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Método que crea un nuevo informe de dependencia
      *
-     * @param  \App\Http\Requests\Storeinforme_dependenciasRequest  $request
+     * @param  \App\Http\Requests\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Storeinforme_dependenciasRequest $request)
-    {
-        //
+    public function store(Request $request) {
+
+        $this->validate($request,[
+            'tipo_dependencia_id'=>'required',
+            'pdf_informe'=>'required|string|max:255',
+            'fecha_informe'=>'required',
+            'observaciones'=>'required|string|max:255',
+        ]);
+
+        $informe_dependencias = new informe_dependencia();
+        $informe_dependencias->tipo_dependencia = $request->tipo_dependencia;
+        $informe_dependencias->expediente_id = $request->expediente_id;
+        $informe_dependencias->pdf_informe = $request->pdf_informe;
+        $informe_dependencias->fecha_informe = $request->fecha_informe;
+        $informe_dependencias->observaciones = $request->observaciones;
+
+        if($informe_dependencias.save()){
+            $log = new LogsInformeDependenciaController();
+            $log->create($informe_dependencias, 'c');
+            return view('informeDependencia.informeDependencia');
+        }
+
+        return back()->with('fail','No se pudo crear el usuario');
     }
 
     /**
-     * Display the specified resource.
+     * Método que me muestra un solo informe de dependencia
      *
      * @param  \App\Models\informe_dependencias  $informe_dependencias
      * @return \Illuminate\Http\Response
      */
-    public function show(informe_dependencias $informe_dependencias)
-    {
-        //
+    public function show($id) {
+        $informe_dependencias = informe_dependencia::find($id);
+        return view('informeDependencia.informeDependencia', ['informe_dependencia', $informe_dependencias]);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Método que me edita un informe de dependencia
+     *
+     * @param  \App\Http\Requests\Request  $request
+     * @param  \App\Models\informe_dependencias  $informe_dependencias
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request) {
+        
+        $this->validate($request,[
+            'tipo_dependencia_id'=>'required',
+            'pdf_informe'=>'required|string|max:255',
+            'fecha_informe'=>'required',
+            'observaciones'=>'required|string|max:255',
+        ]);
+
+        $informe_dependencias = informe_dependencia::find($request->id);
+        $log = new LogsInformeDependenciaController();
+
+        $informe_dependencias->tipo_dependencia = $request->tipo_dependencia;
+        $informe_dependencias->expediente_id = $request->expediente_id;
+        $informe_dependencias->pdf_informe = $request->pdf_informe;
+        $informe_dependencias->fecha_informe = $request->fecha_informe;
+        $informe_dependencias->observaciones = $request->observaciones;
+
+        $informe_dependencias->save();
+
+        $log->create($informe_dependencias, 'u');
+
+        return view('informeDependencia.informeDependencia');
+    }
+
+    /**
+     * Método que elimina un estado de dependencia determinado
      *
      * @param  \App\Models\informe_dependencias  $informe_dependencias
      * @return \Illuminate\Http\Response
      */
-    public function edit(informe_dependencias $informe_dependencias)
-    {
-        //
-    }
+    public function destroy($id) {
+        $informe_dependencias = informe_dependencia::find($request->id);
+        $log = new LogsInformeDependenciaController();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\Updateinforme_dependenciasRequest  $request
-     * @param  \App\Models\informe_dependencias  $informe_dependencias
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Updateinforme_dependenciasRequest $request, informe_dependencias $informe_dependencias)
-    {
-        //
-    }
+        $informe_dependencias->delete();
+        
+        $log->create($informe_dependencias, 'd');
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\informe_dependencias  $informe_dependencias
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(informe_dependencias $informe_dependencias)
-    {
-        //
+        return view('informeDependencia.informeDependencia');
     }
 }
--- app/Http/Controllers/LogsInformeDependenciaController.php
+++ app/Http/Controllers/LogsInformeDependenciaController.php
@@ -7,81 +7,33 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Storelogs_informe_dependenciaRequest;
 use App\Http\Requests\Updatelogs_informe_dependenciaRequest;
 
-class LogsInformeDependenciaController extends Controller
-{
+class LogsInformeDependenciaController extends Controller {
+    
     /**
-     * Display a listing of the resource.
-     *
+     * Método que crea un objeto sobre la tabla
+     * logs_informes_dependencias, por cada consulta
+     * realizada sobre la misma
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    public function create($informe_dependencia, $char) {
+        
+        $logs_informe_dependencia = new logs_informe_dependencia();
+        $user = auth()->user();
+
+        $logs_informe_dependencia->informe_dependencia_id = $informe_dependencia->id;
+        $logs_informe_dependencia->tipo_dependencia_id = $informe_dependencia->tipo_dependencia_id;
+        $logs_informe_dependencia->expediente_id = $informe_dependencia->expediente_id;
+        $logs_informe_dependencia->pdf_informe = $informe_dependencia->pdf_informe;
+        $logs_informe_dependencia->fecha_informe = $informe_dependencia->fecha_informe;
+        $logs_informe_dependencia->observaciones = $informe_dependencia->observaciones;
+        $logs_informe_dependencia->accion = $char;
+        //$logs_informe_dependencia->usuario_id = $user->id; -> PORBAR CON USUARIO
+        //$logs_informe_dependencia->usuario_nombre = $user->usuario; -> IDEM ANTERIOR
+
+        $logs_informe_dependencia->save();
+        
+        return 'guardado';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\Storelogs_informe_dependenciaRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Storelogs_informe_dependenciaRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\logs_informe_dependencia  $logs_informe_dependencia
-     * @return \Illuminate\Http\Response
-     */
-    public function show(logs_informe_dependencia $logs_informe_dependencia)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\logs_informe_dependencia  $logs_informe_dependencia
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(logs_informe_dependencia $logs_informe_dependencia)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\Updatelogs_informe_dependenciaRequest  $request
-     * @param  \App\Models\logs_informe_dependencia  $logs_informe_dependencia
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Updatelogs_informe_dependenciaRequest $request, logs_informe_dependencia $logs_informe_dependencia)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\logs_informe_dependencia  $logs_informe_dependencia
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(logs_informe_dependencia $logs_informe_dependencia)
-    {
-        //
-    }
 }
