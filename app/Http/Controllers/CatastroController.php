7dd1c6dfc78fbb37773d85f17d0792effe42b3e7
sergioyanez <sergiomyanez02@gmail.com>
2023-02-02 01:55:51 -0300

    Se pone a revision CatastroController

--- app/Http/Controllers/CatastroController.php
+++ app/Http/Controllers/CatastroController.php
@@ -3,84 +3,153 @@
 namespace App\Http\Controllers;
 
 use App\Models\Catastro;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreCatastroRequest;
 use App\Http\Requests\UpdateCatastroRequest;
+use App\Http\Controllers\LogsCatastroController;
 
-class CatastroController extends Controller
-{
+class CatastroController extends Controller {
+
     /**
-     * Display a listing of the resource.
+     * Método que muestra la lista de todos
+     * los catastros que hay en la base de datos
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    public function index() {
+
+        $catastro = Catastro::all();
+        return view('catastro.catastro', ['catastros'=>$catastro]);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Muestra un formulario para crear un catastro
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create() {
+        return view('catastro.crear');
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCatastroRequest  $request
+     * Método que crea un nuevo Catastro
+     * @param  \App\Http\Requests\UpdateCatastroRequest  $request
+     * @param  \App\Http\Requests\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreCatastroRequest $request)
-    {
-        //
+    public function store(Request $request) {
+
+        $this->validate($request,[
+            'circunscripcion'=>'required|string|max:10',
+            'seccion'=>'required|string|max:10',
+            'chacra'=>'required|string|max:10',
+            'quinta'=>'required|string|max:10',
+            'fraccion'=>'required|string|max:10',
+            'manzana'=>'required|string|max:10',
+            'parcela'=>'required|string|max:10',
+            'sub_parcela'=>'required|string|max:10',
+            'observacion'=>'required|string|max:10',
+            'fecha_informe'=>'required',
+            'pdf_informe'=>'required|string|max:255',
+         ]);
+
+
+        $catastro = new Catastro();
+        $catastro->id = $request->id;
+        $catastro->circunscripcion = $request->circunscripcion;
+        $catastro->seccion = $request->seccion;
+        $catastro->chacra = $request->chacra;
+        $catastro->quinta = $request->quinta;
+        $catastro->fraccion = $request->fraccion;
+        $catastro->manzana = $request->manzana;
+        $catastro->parcela = $request->parcela;
+        $catastro->sub_parcela = $request->sub_parcela;
+        $catastro->observacion = $request->observacion;
+        $catastro->fecha_informe = $request->fecha_informe;
+        $catastro->pdf_informe = $request->pdf_informe;
+
+        if($catastro->save()){
+            $log = new LogsCatastroController();
+            $log->create($catastro, 'c');
+            return redirect()->view('catastro.catastro');
+        }
+
+        return back()->with('fail','No se pudo cargar el catastro');
     }
 
+
     /**
-     * Display the specified resource.
+     * Muestra un solo catastro
      *
      * @param  \App\Models\Catastro  $catastro
      * @return \Illuminate\Http\Response
      */
-    public function show(Catastro $catastro)
-    {
-        //
+    public function show($id){
+
+        $catastro = Catastro::find($id);
+        return view('catastro.catastro', ['catastro'=>$catastro]);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Catastro  $catastro
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Catastro $catastro)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Método para editar un catastro existente
      *
      * @param  \App\Http\Requests\UpdateCatastroRequest  $request
      * @param  \App\Models\Catastro  $catastro
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateCatastroRequest $request, Catastro $catastro)
-    {
-        //
+    public function update(Request $request) {
+
+        $this->validate($request,[
+            'circunscripcion'=>'required|string|max:10',
+            'seccion'=>'required|string|max:10',
+            'chacra'=>'required|string|max:10',
+            'quinta'=>'required|string|max:10',
+            'fraccion'=>'required|string|max:10',
+            'manzana'=>'required|string|max:10',
+            'parcela'=>'required|string|max:10',
+            'sub_parcela'=>'required|string|max:10',
+            'observacion'=>'required|string|max:10',
+            'fecha_informe'=>'required',
+            'pdf_informe'=>'required|string|max:255',
+         ]);
+
+        $log = new LogsCatastroController();
+
+        $catastro = Catastro::find($request->id);
+        $catastro->circunscripcion = $request->circunscripcion;
+        $catastro->seccion = $request->seccion;
+        $catastro->chacra = $request->chacra;
+        $catastro->quinta = $request->quinta;
+        $catastro->fraccion = $request->fraccion;
+        $catastro->manzana = $request->manzana;
+        $catastro->parcela = $request->parcela;
+        $catastro->sub_parcela = $request->sub_parcela;
+        $catastro->observacion = $request->observacion;
+        $catastro->fecha_informe = $request->fecha_informe;
+        $catastro->pdf_informe = $request->pdf_informe;
+
+        $catastro->save();
+
+        $log->create($catastro, 'u');
+
+        return redirect()->view('catastro.catastro');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Método para eliminar un catastro existente
      *
      * @param  \App\Models\Catastro  $catastro
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Catastro $catastro)
-    {
-        //
+    public function destroy($id) {
+
+        $log = new LogsCatastroController();
+
+        $catastro = Catastro::find($id);
+        $catastro->delete();
+
+        $log->create($catastro, 'd');
+
+        return redirect()->view('catastro.catastro');
     }
 }
