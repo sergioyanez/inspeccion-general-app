0dee082707a7da57ce303c4cabd95dbd67b986be
ElvitaKehler <mekdy.20@gmail.com>
2023-02-03 10:36:51 -0300

    se agrega us15 Tablas Contribuyente y Log

--- app/Http/Controllers/ContribuyenteController.php
+++ app/Http/Controllers/ContribuyenteController.php
@@ -3,74 +3,113 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contribuyente;
+use App\Models\Tipo_dni;
+use App\Models\Estado_civil;
+use App\Http\Controllers\LogsContribuyenteController;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreContribuyenteRequest;
 use App\Http\Requests\UpdateContribuyenteRequest;
 
 class ContribuyenteController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Muesta todos los contribuyentes
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $contribuyentes = Contribuyente::all();
+        return view('contribuyente.contribuyentes', ['contribuyentes' => $contribuyentes]);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Muestra un formulario para crear un contribuyente
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        $estadosCivil = Estado_civil::all();
+        $tiposDni = Tipo_dni::all();
+        return view('contribuyente.crear', ['estados'=>$estadosCivil, 'tipos'=>$tiposDni]);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Guarda el contribuyente creado en la base de datos
      *
-     * @param  \App\Http\Requests\StoreContribuyenteRequest  $request
+     * @param  \App\Http\Requests\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreContribuyenteRequest $request)
+    public function store(Request $request)
     {
-        //
+        $log = new LogsContribuyenteController();
+
+        $contribuyente = new contribuyente();
+        $contribuyente->tipo_dni_id = $request->tipo_dni_id;
+        $contribuyente->estado_civil_id = $request->estado_civil_id;
+        $contribuyente->cuit = $request->cuit;
+        $contribuyente->ingresos_brutos = $request->ingresos_brutos;
+        $contribuyente->nombre = $request->nombre;
+        $contribuyente->apellido = $request->apellido;
+        $contribuyente->dni = $request->dni;
+        $contribuyente->fecha_nacimiento = $request->fecha_nacimiento;
+        $contribuyente->telefono = $request->telefono;
+        $contribuyente->nombre_conyuge = $request->nombre_conyuge;
+        $contribuyente->apellido_conyuge = $request->apellido_conyuge;
+        $contribuyente->dni_conyuge = $request->dni_conyuge;
+
+        $contribuyente->save();
+
+        $log->store($contribuyente, 'c');
+
+        return redirect()->route('contribuyentes');
     }
 
     /**
-     * Display the specified resource.
+     * Muesta un contribuyente en un formulario con todos los campos cargados
      *
      * @param  \App\Models\Contribuyente  $contribuyente
      * @return \Illuminate\Http\Response
      */
-    public function show(Contribuyente $contribuyente)
+    public function show($id)
     {
-        //
+        $contribuyente = contribuyente::find($id);
+        $estadosCivil = Estado_civil::all();
+        $tiposDni = Tipo_dni::all();
+        return view('contribuyente.mostrar', ['contribuyente' => $contribuyente, 'estados'=>$estadosCivil, 'tipos'=>$tiposDni]);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Contribuyente  $contribuyente
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Contribuyente $contribuyente)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Guarda los datos actualizados del contribuyente
      *
      * @param  \App\Http\Requests\UpdateContribuyenteRequest  $request
      * @param  \App\Models\Contribuyente  $contribuyente
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateContribuyenteRequest $request, Contribuyente $contribuyente)
+    public function update(Request $request)
     {
-        //
+        $log = new LogsContribuyenteController();
+
+        $contribuyente = contribuyente::find($request->contribuyente_id);
+        $contribuyente->tipo_dni_id = $request->tipo_dni_id;
+        $contribuyente->estado_civil_id = $request->estado_civil_id;
+        $contribuyente->cuit = $request->cuit;
+        $contribuyente->ingresos_brutos = $request->ingresos_brutos;
+        $contribuyente->nombre = $request->nombre;
+        $contribuyente->apellido = $request->apellido;
+        $contribuyente->dni = $request->dni;
+        $contribuyente->fecha_nacimiento = $request->fecha_nacimiento;
+        $contribuyente->telefono = $request->telefono;
+        $contribuyente->nombre_conyuge = $request->nombre_conyuge;
+        $contribuyente->apellido_conyuge = $request->apellido_conyuge;
+        $contribuyente->dni_conyuge = $request->dni_conyuge;
+
+        $contribuyente->save();
+
+        $log->store($contribuyente, 'u');
+
+        return redirect()->route('contribuyentes');
     }
 
     /**
@@ -79,8 +118,17 @@ class ContribuyenteController extends Controller
      * @param  \App\Models\Contribuyente  $contribuyente
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Contribuyente $contribuyente)
+    public function destroy($id)
     {
-        //
+        $log = new LogsContribuyenteController();
+
+        $contribuyente = Contribuyente::find($id);
+        $contribuyente->delete();
+
+        $log->store($contribuyente, 'd');
+
+        return redirect()->route('contribuyentes');
+
+        
     }
 }
--- app/Http/Controllers/LogsContribuyenteController.php
+++ app/Http/Controllers/LogsContribuyenteController.php
@@ -9,79 +9,39 @@ use App\Http\Requests\Updatelogs_contribuyenteRequest;
 
 class LogsContribuyenteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    
+
+    
+    // Crea un nuevo registro de log contribuyente
+    public function store($contribuyente, $char)
     {
-        //
+        $logs_contribuyente = new logs_contribuyente();
+        $user = auth()->user();
+
+        $logs_contribuyente->contribuyente_id = $contribuyente->id;
+        $logs_contribuyente->tipo_dni_id = $contribuyente->tipo_dni_id;
+        $logs_contribuyente->estado_civil_id = $contribuyente->estado_civil_id;
+        $logs_contribuyente->cuit = $contribuyente->cuit;
+        $logs_contribuyente->ingresos_brutos = $contribuyente->ingresos_brutos;
+        $logs_contribuyente->nombre = $contribuyente->nombre;
+        $logs_contribuyente->apellido = $contribuyente->apellido;
+        $logs_contribuyente->dni = $contribuyente->dni;
+        $logs_contribuyente->fecha_nacimiento = $contribuyente->fecha_nacimiento;
+        $logs_contribuyente->telefono = $contribuyente->telefono;
+        $logs_contribuyente->nombre_conyuge = $contribuyente->nombre_conyuge;
+        $logs_contribuyente->apellido_conyuge = $contribuyente->apellido_conyuge;
+        $logs_contribuyente->dni_conyuge = $contribuyente->dni_conyuge;
+        $logs_contribuyente->accion = $char;
+        $logs_contribuyente->usuario_id = $user->id; // PROBAR CON USUARIO
+        $logs_contribuyente->usuario_nombre = $user->usuario; // IDEM ANTERIOR
+
+        $logs_contribuyente->save();
+        
+        //return 'guardado';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\Storelogs_contribuyenteRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Storelogs_contribuyenteRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\logs_contribuyente  $logs_contribuyente
-     * @return \Illuminate\Http\Response
-     */
-    public function show(logs_contribuyente $logs_contribuyente)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\logs_contribuyente  $logs_contribuyente
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(logs_contribuyente $logs_contribuyente)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\Updatelogs_contribuyenteRequest  $request
-     * @param  \App\Models\logs_contribuyente  $logs_contribuyente
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Updatelogs_contribuyenteRequest $request, logs_contribuyente $logs_contribuyente)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\logs_contribuyente  $logs_contribuyente
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(logs_contribuyente $logs_contribuyente)
-    {
-        //
-    }
+    
+    
 }
