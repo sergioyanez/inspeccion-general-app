5e81b9dc960de4291ce5e93fa087a3c7099d4ae7
seba <sebaesains77@gmail.com>
2023-02-04 20:19:01 -0300

    Se modifico la vista de expedientes

--- resources/views/expediente/expedientes.blade.php
+++ resources/views/expediente/expedientes.blade.php
@@ -13,42 +13,45 @@
             </div>
 
         </form>
-      
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Nº DE COMERCIO</th>
-                    <th>CONTRIBUYENTE</th>
-                    <th>OBSERVACIONES/DETALLES</th>
-                    <th>DETALLE DE LA HABILITACION</th>
-                    <th>ACCIONES</th>                  
-                </tr>
-            </thead>
-            <tbody class="table-border-bottom-0">
-            
-                @foreach($expedientes as $expediente)
-                    <tr>
-                        <td>{{$expediente->nro_comercio}}</td>
-                        <td>
-                            @foreach($expediente->contribuyentes as $registro)
-                                {{$registro->nombre}} 
-                                {{$registro->apellido}}
-                                -
-                            @endforeach
 
-                            @foreach($expediente->personasJuridicas as $registro1)
-                                {{$registro1->nombre_representante}}
-                                {{$registro1->apellido_representante}}
-                                -
-                            @endforeach
-                        </td>
-                        <td>{{$expediente->observaciones_grales}}</td>
-                        <td>{{$expediente->detalleHabilitacion->tipoHabilitacion->descripcion}}</td>
-                        <td><a href="{{route('expedientes-mostrar', $expediente->id)}}">Ver màs</a></td> 
+        @if(request('buscarporcomercio'))
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nº DE COMERCIO</th>
+                        <th>CONTRIBUYENTE</th>
+                        <th>OBSERVACIONES/DETALLES</th>
+                        <th>DETALLE DE LA HABILITACION</th>
+                        <th>ACCIONES</th>                  
                     </tr>
-                @endforeach   
-            </tbody>
-        </table>
-        
-     </div>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                
+                    @forelse($expedientes as $expediente)
+                        <tr>
+                            <td>{{$expediente->nro_comercio}}</td>
+                            <td>
+                                @foreach($expediente->contribuyentes as $registro)
+                                    {{$registro->nombre}} 
+                                    {{$registro->apellido}}
+                                    -
+                                @endforeach
+
+                                @foreach($expediente->personasJuridicas as $registro1)
+                                    {{$registro1->nombre_representante}}
+                                    {{$registro1->apellido_representante}}
+                                    -
+                                @endforeach
+                            </td>
+                            <td>{{$expediente->observaciones_grales}}</td>
+                            <td>{{$expediente->detalleHabilitacion->tipoHabilitacion->descripcion}}</td>
+                            <td><a href="{{route('expedientes-mostrar', $expediente->id)}}">Ver màs</a></td> 
+                        </tr>
+                    @empty
+                        <h2>No se encontraron Expedientes</h2>
+                    @endforelse    
+                </tbody>
+            </table>
+        @endif 
+    </div>
 </div>
