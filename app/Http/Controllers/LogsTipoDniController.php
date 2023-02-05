f91aa654300758e8255df1ed5e9545c8157e1376
ElvitaKehler <mekdy.20@gmail.com>
2023-02-05 19:40:15 -0300

    se agrega us1 Tipo_dni y se modifican datos de prueba en expediente

--- app/Http/Controllers/LogsTipoDniController.php
+++ app/Http/Controllers/LogsTipoDniController.php
@@ -8,8 +8,8 @@ use App\Http\Requests\Storelogs_tipo_dniRequest;
 use App\Http\Requests\Updatelogs_tipo_dniRequest;
 
 
-class LogsTipoDniController extends Controller
-{
+class LogsTipoDniController extends Controller {
+
     /**
      * Display a listing of the resource.
      *
@@ -33,8 +33,8 @@ class LogsTipoDniController extends Controller
         $logs_tipo_dni->tipo_dni_id = $tipo_dni->id;
         $logs_tipo_dni->descripcion = $tipo_dni->descripcion;
         $logs_tipo_dni->accion = $char;
-        $logs_tipo_dni->usuario_id = $user->id;
-        $logs_tipo_dni->usuario_nombre = $user->usuario;
+        //$logs_tipo_dni->usuario_id = $user->id;
+        //$logs_tipo_dni->usuario_nombre = $user->usuario;
 
         $logs_tipo_dni->save();
 
