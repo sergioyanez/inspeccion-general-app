fdbe55a2a1bd166acfbedb5072ccaf8d495396f4
Euge Miller <eugeniomiller93@gmail.com>
2023-02-23 16:13:42 -0300

    Botones usuario administrador

--- resources/views/paginaPrincipal/botonesInicio.blade.php
+++ resources/views/paginaPrincipal/botonesInicio.blade.php
@@ -1,5 +1,6 @@
 <div class="container">
     <div class="row">
+        
         <div class="col-12 col-lg-6 dropend">
         <a class="btn btnPrincipal btnExpediente d-flex align-items-center justify-content-center" href="{{route('expedientes-crear')}}">Registrar expediente</a>
         <a class="btn btnPrincipal btnBuscar d-flex align-items-center justify-content-center" href="{{route('busqueda-expediente')}}">Buscar / Editar expediente</a>
@@ -12,8 +13,10 @@
             <li><a class="dropdown-item" href="#">Habilitaciones vencidas</a></li>
         </ul>
         </div>
-        <div class="col-12 col-lg-6">
-            <a class="btn btnPrincipal btnAdministracion d-flex align-items-center justify-content-center" href="{{route('usuarios')}}">Administrar usuarios</a>
-        </div>
+        @if(Auth::user()->tipo_permiso_id == 1)
+            <div class="col-12 col-lg-6">
+                <a class="btn btnPrincipal btnAdministracion d-flex align-items-center justify-content-center" href="{{route('usuarios')}}">Administrar usuarios</a>
+            </div>
+        @endif
     </div>
 </div>
