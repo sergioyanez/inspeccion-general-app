a7a553c89c03d4b9ff2972f587f8a2f54e334546
seba <sebaesains77@gmail.com>
2023-02-26 18:04:47 -0300

    js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,29 @@
+'use strict';
 import "./bootstrap";
+document.addEventListener("DOMContentLoaded", function() {
 
-let tipo = document.getElementById("tipo_inmueble");
-let caja = document.getElementById("fecha_alquiler");
-cajaFechaVencimiento("none");
+    let tipo = document.getElementById("tipo_inmueble");
+    let caja = document.getElementById("fecha_alquiler");
+    let fechaVencimiento = document.getElementById("fechaVencimiento");
+    cajaFechaVencimiento("none");
 
-function cajaFechaVencimiento(d) {
-    if (caja) {
-        caja.style.display = d;
-    }
-}
-
-if (tipo) {
-    tipo.addEventListener("change", function () {
-        if (Number(this.value) === 1) {
-            cajaFechaVencimiento("block");
-        } else {
-            cajaFechaVencimiento("none");
+    function cajaFechaVencimiento(d) {
+        if (caja) {
+            caja.style.display = d;
         }
-    });
-}
+    }
+
+    if (tipo) {
+        tipo.addEventListener("change", function() {
+            if (Number(this.value) === 1) {
+                cajaFechaVencimiento("block");
+                fechaVencimiento.required = true;
+            } else {
+                cajaFechaVencimiento("none");
+                fechaVencimiento.required = false;
+            }
+        });
+    }
+});
+
+
