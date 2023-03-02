92c189b1a9e89fb806991b3c20cd78b274eddb90
Euge Miller <eugeniomiller93@gmail.com>
2023-03-02 18:28:04 -0300

    js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,81 @@
-'use strict';
 import "./bootstrap";
-document.addEventListener("DOMContentLoaded", function() {
+import "../scss/app.scss";
+import * as bootstrap from "bootstrap";
 
-    let tipo = document.getElementById("tipo_inmueble");
-    let caja = document.getElementById("fecha_alquiler");
-    let fechaVencimiento = document.getElementById("fechaVencimiento");
-    cajaFechaVencimiento("none");
+// ES6 Modules or TypeScript
+import Swal from "sweetalert2";
 
-    function cajaFechaVencimiento(d) {
-        if (caja) {
-            caja.style.display = d;
-        }
-    }
+const btnsDelete = document.getElementsByClassName("btnsDelete");
 
-    if (tipo) {
-        tipo.addEventListener("change", function() {
-            if (Number(this.value) === 1) {
-                cajaFechaVencimiento("block");
-                fechaVencimiento.required = true;
-            } else {
-                cajaFechaVencimiento("none");
-                fechaVencimiento.required = false;
-            }
+if (btnsDelete) {
+    for (let i = 0; i < btnsDelete.length; i++) {
+        btnsDelete[i].addEventListener("click", () => {
+            alert(btnsDelete[i].value);
         });
     }
+}
+
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: "btn btn-orange ms-2",
+        cancelButton: "btn btn-secundary",
+    },
+    buttonsStyling: false,
 });
+
+function alert(id) {
+    swalWithBootstrapButtons
+        .fire({
+            title: "¿Estas seguro?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Confirmar",
+            cancelButtonText: "Volver",
+            reverseButtons: true,
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+                window.location =
+                    "http://127.0.0.1:8000/usuario/eliminar/" + id;
+            } else {
+                Swal.DismissReason.cancel;
+            }
+        });
+}
+
+const divModal = document.getElementById("example");
+
+if (divModal) {
+    const myModalEl = new bootstrap.Modal(divModal);
+    if (myModalEl) {
+        myModalEl.show();
+    }
+}
+
+if (!divModal) {
+    const ExistmodalPerfil = document.getElementById("ExistmodalPerfil");
+
+    if (ExistmodalPerfil && modalperfil) {
+        const modalperfil = document.getElementById("modalperfil");
+        const myModalPerfil = new bootstrap.Modal(modalperfil);
+        if (myModalPerfil) {
+            myModalPerfil.show();
+        }
+    }
+}
+
+const inputSuccess = document.getElementById("alertSuccess");
+
+if (inputSuccess) {
+    alertSuccess(inputSuccess.value);
+}
+
+function alertSuccess(mensaje) {
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: mensaje,
+        showConfirmButton: false,
+        timer: 1500,
+    });
+}
