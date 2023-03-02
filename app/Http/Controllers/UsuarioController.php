4ee2b6beb10d06b56e7c16e5f4f1b29e05fbac7e
Euge Miller <eugeniomiller93@gmail.com>
2023-03-02 18:25:48 -0300

    userController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\LogsUsuarioController;
 use App\Http\Requests\StoreUsuarioRequest;
 use App\Http\Requests\UpdateUsuarioRequest;
-
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Validation\ValidationException;
 
 class UsuarioController extends Controller {
 
@@ -28,7 +29,8 @@ class UsuarioController extends Controller {
      */
     public function create() {
         $tiposPermisos = Tipo_permiso::all();
-        return view('usuario.crear', ['tiposPermisos'=>$tiposPermisos]);
+        $usuario = User::all();
+        return view('usuario.crear', ['tiposPermisos'=>$tiposPermisos,'usuarios' => $usuario]);
     }
 
     /**
@@ -45,7 +47,7 @@ class UsuarioController extends Controller {
         if($usuario->save()) {
             $log = new LogsUsuarioController();
             $log->store($usuario, 'c');
-            return redirect()->route('usuarios');
+            return redirect()->route('usuarios')->with('success','Creado con éxito');
         }
         return back()->with('fail','No se pudo crear el usuario');
     }
@@ -57,10 +59,20 @@ class UsuarioController extends Controller {
      * @param  int $id
      */
     public function show($id){
-
         $usuario = User::find($id);
         $tiposPermisos = Tipo_permiso::all();
         return view('usuario.mostrar', ['usuario' => $usuario, 'tiposPermisos'=>$tiposPermisos]);
+    }
+
+     /**
+     * Muestra un formulario para editar un usuario
+     * @param  int $id
+     */
+    public function edit($id) {
+        $usuario = User::find($id);
+        $tiposPermisos = Tipo_permiso::all();
+        $usuarios = User::all();
+        return view('usuario.crear', ['tiposPermisos'=>$tiposPermisos,'usuarios' => $usuarios, 'usuario'=>$usuario]);
     }
 
     /**
@@ -68,18 +80,60 @@ class UsuarioController extends Controller {
      * @param  \App\Http\Requests\UpdateUsuarioRequest  $request
      */
     public function update(UpdateUsuarioRequest $request) {
+        $request->validate([
+            'usuario' => 'required|unique:users,usuario,'.$request->usuario_id,
+            'email' => 'required|unique:users,email,'.$request->usuario_id,
+        ]);
         $usuario = User::find($request->usuario_id);
         $usuario->usuario = $request->usuario;
         $usuario->tipo_permiso_id = $request->tipo_permiso_id;
         $usuario->email = $request->email;
-        $usuario->password = Hash::make($request->password);
+        //$usuario->password = Hash::make($request->password);
 
         if($usuario->save()){
             $log = new LogsUsuarioController();
             $log->store($usuario, 'u');
-            return redirect()->route('usuarios');
+            return redirect()->route('usuarios')->with('success','Editado con éxito');
         }
         return back()->with('fail','No se pudo actualizar el usuario');
+
+    }
+
+    /**
+     * Método para editar perfil de usuario
+     * @param  \App\Http\Requests\UpdateUsuarioRequest  $request
+     */
+    public function updateFace(Request $request) {
+        $request->validate([
+            'usuarioFace' => 'required|unique:users,usuario,'.$request->usuario_id_face,
+            'usuario_id_face' => 'required',
+            'passwordFace' => 'required',
+        ]);
+        $usuario = User::find($request->usuario_id_face);
+        if(isset($request->newPasswordFace)){
+            if(Hash::check($request->passwordFace,$usuario->password)){
+                $request->validate([
+                    'newPasswordFace' => 'required|min:8',
+                    'repetirPasswordFace' => 'required|same:newPasswordFace',
+                ]);
+                $usuario->password = Hash::make($request->newPasswordFace);
+            }
+            else{
+                throw ValidationException::withMessages([
+                    'passwordFace' => 'Contraseña no válida.',
+                    'failFace' => 'No se pudo actualizar el perfil',
+                ]);
+            }
+        }
+        $usuario->usuario = $request->usuarioFace;
+
+        if($usuario->save()){
+            $log = new LogsUsuarioController();
+            $log->store($usuario, 'u');
+            return back()->with('success','Editado con éxito');
+        }
+        return back()->with('failFace','No se pudo actualizar el usuario');
+     
 
     }
 
@@ -93,7 +147,7 @@ class UsuarioController extends Controller {
         if($usuario->delete()){
             $log = new LogsUsuarioController();
             $log->store($usuario, 'd');
-            return redirect()->route('usuarios');
+            return redirect()->route('usuarios')->with('success','Eliminado con éxito');
         }
         return back()->with('fail','No se pudo eliminar el usuario');
     }
