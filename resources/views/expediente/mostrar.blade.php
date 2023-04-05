99d07273fcc717bd260586db76cf341a682188fe
sergioyanez <sergiomyanez02@gmail.com>
2023-04-04 22:40:15 -0300

    se corrige codigo

--- resources/views/expediente/mostrar.blade.php
+++ resources/views/expediente/mostrar.blade.php
@@ -1,6 +1,6 @@
 @include('header.header')
     <div class="container mb-3">
-        
+
 
         <div class="row">
             <div class="col-xl-12">
@@ -33,20 +33,20 @@
                     <!--d-flex justify-content-between align-items-center-->
                     <div class="card-body">
                         <div class="">
-                            <p class="mb-1"><b>Titulares personas físicas:</b> 
+                            <p class="mb-1"><b>Titulares personas físicas:</b>
                                 @if(!isset($expedientesContribuyentes))
                                     <span class="text-muted">Sin personas físicas cargadas..</span>
                                 @else
                                     @forelse ($expedientesContribuyentes as $expedContrib)
                                             @if ($expedContrib->expediente_id ==$expediente->id)
                                             <span class="p-2 m-1">  {{$expedContrib->contribuyente->nombre}}  {{$expedContrib->contribuyente->apellido}} /
-                                                {{$expedContrib->contribuyente->dni}} <a href="{{route('expedientesContribuyentes-eliminar', $expedContrib->id)}}"class="ms-2 btn btn-danger btn-sm">Eliminar</a> 
+                                                {{$expedContrib->contribuyente->dni}} <a href="{{route('expedientesContribuyentes-eliminar', $expedContrib->id)}}"class="ms-2 btn btn-danger btn-sm">Eliminar</a>
                                             </span>
                                             @endif
                                         @empty
                                     @endforelse
                                 @endif
-                                <hr class="p-0 m-0"> 
+                                <hr class="p-0 m-0">
                             </p>
                         </div>
                         <div class="row border p-2">
@@ -61,11 +61,11 @@
                                     <div class="col-3 m-0">
                                         <input  class="btn btn-orange" type="submit" value="Buscar">
                                     </div>
-                                    
+
                                 </form>
                             </div>
 
-                            <div class="col-6"> 
+                            <div class="col-6">
 
                                 {{-- MUESTRA EL CONTRIBUYENTE SI LO ENCONTRO Y DA LA OPCION DE AGREGARLO AL EXPEDIENTE
                                 O SI NO LO ENCONTRO DA LA OPCION DE CREARLO --}}
@@ -83,7 +83,7 @@
                                                         <input type="hidden" name="idExpSiguiente" value="{{$expediente->id}}">
                                                         <button class="ms-3 btn-sm btn btn-primary"type="submit">Agregar</button>
                                                     </li>
-                                            
+
                                                     @foreach ($expedientesContribuyentes as $ec)
                                                         @if($ec->expediente_id == $expediente->id && $ec->contribuyente_id == $contribuyente->id)
                                                             <p class="text-danger">El contribuyente ya esta cargado en el expediente</p>
@@ -102,24 +102,24 @@
                                     @endisset
                                 </form>
                             </div>
-                            
+
                         </div>
 
                         <div class="mt-5">
-                            <p class="mb-1"><b>Titulares personas jurídicas:</b> 
+                            <p class="mb-1"><b>Titulares personas jurídicas:</b>
                                 @if(!isset($expedientesPersonasJuridicas))
                                     <span class="text-muted">Sin personas jurídicas cargadas..</span>
                                 @else
                                     @forelse ($expedientesPersonasJuridicas as $expedPersJurid)
                                             @if ($expedPersJurid->expediente_id ==$expediente->id)
-                                            <span class="p-2 m-1">  {{$expedPersJurid->personaJuridica->nombre_representante}}  {{$expedPersJurid->personaJuridica->apellido_representante}} /
-                                                {{$expedPersJurid->personaJuridica->dni_representante}} <a  href="{{route('expedientesPersonasJuridicas-eliminar', $expedPersJurid->id)}}"class="ms-2 btn btn-danger btn-sm">Eliminar</a>
+                                            <span class="p-2 m-1">  {{$expedPersJurid->personaJuridica->nombre_persona_juridica}} / {{$expedPersJurid->personaJuridica->cuit}}
+                                               <a  href="{{route('expedientesPersonasJuridicas-eliminar', $expedPersJurid->id)}}"class="ms-2 btn btn-danger btn-sm">Eliminar</a>
                                             </span>
                                             @endif
                                         @empty
                                     @endforelse
                                 @endif
-                                <hr class="p-0 m-0"> 
+                                <hr class="p-0 m-0">
                             </p>
                         </div>
 
@@ -137,7 +137,7 @@
                                     </div>
                                 </form>
                             </div>
-                            
+
                             <div class="col-6">
                                 {{-- MUESTRA LA PERSONA JURIDICA SI LA ENCONTRO Y DA LA OPCION DE AGREGARLA AL EXPEDIENTE
                                 O SI NO LA ENCONTRO DA LA OPCION DE CREARLA --}}
@@ -148,7 +148,7 @@
                                         <h5 class="mb-3 text-left font-weight">Agregar al expediente la persona jurídica:</h5>
                                             <ul class="list-group">
                                                 @foreach ($personasJuridicas as $pj)
-                                                <li class="list-group-item list-group-item-action" data-toggle="list">{{$pj->nombre_representante}}
+                                                <li class="list-group-item list-group-item-action" data-toggle="list">
                                                     {{-- {{$pj->apellido_representante}} / {{$pj->dni_representante}} --}}
                                                     {{$pj->nombre_persona_juridica}} / {{$pj->cuit}}
                                                     <input type="hidden" name="persona_juridica_id" value="{{$pj->id}}">
@@ -174,7 +174,7 @@
                             </div>
                         </div>
                     </div>
-                    
+
                     {{-- PRIMER PARTE DE CARGA DE EXPEDIENTE. PRIMER PAGINA DEL FIGMA --}}
                     <div class="card-body">
 
@@ -214,7 +214,7 @@
                             <input type="hidden" name="nro_expediente" value="{{$expediente->nro_expediente}}">
 
                             <input type="hidden" name="nro_comercio" value="{{$expediente->nro_comercio}}">
-                         
+
                             <div class="col-12 row mb-4">
                                 <div class="col-3 mb-3">
                                     <label class="form-label" for="basic-default-fullname">Actividad principal</label>
@@ -228,21 +228,21 @@
                                     <label class="form-label" for="basic-default-fullname">Anexo</label>
                                     <input value="{{$expediente->anexo}}" type="text" name="anexo" class="form-control" id="basic-default-nombreCompleto" />
                                     @error('anexo')
-                                        
+
                                         <div>
                                             {{$message}}
                                         </div>
-                                    @enderror 
+                                    @enderror
                                 </div>
                             </div>
-                           
-                            
+
+
                             {{-- DATOS DEL INMUEBLE --}}
-                            
+
                                 <div class="row col-12 mb-5">
                                     <input type="hidden" name="inmueble_id" value="{{$expediente->detalleInmueble->inmueble->id}}">
                                     <label class="form-label" for="basic-default-fullname">Domicilio/Inmueble</label>
-                                 
+
                                         <div class="col-3">
                                             <label class="form-label" for="basic-default-fullname">Calle</label>
                                             <input value="{{$expediente->detalleInmueble->inmueble->calle}}" type="text" name="calle" class="form-control @error('calle') is-invalid @enderror" id="basic-default-nombreCompleto" />
@@ -253,8 +253,8 @@
                                                 </div>
                                             @enderror
                                         </div>
-                            
-                                
+
+
                                     <div class="col-2">
                                         <label class="form-label" for="basic-default-fullname">Número</label>
                                         <input value="{{$expediente->detalleInmueble->inmueble->numero}}" type="text" name="numero" class="form-control @error('numero') is-invalid @enderror" id="basic-default-nombreCompleto" />
@@ -265,7 +265,7 @@
                                             </div>
                                         @enderror
                                     </div>
-                                
+
                                     <input type="hidden" name="detalle_inmueble_id" value="{{$expediente->detalleInmueble->id}}">
                                     <div class="col-3">
                                         <label class="form-label" for="basic-default-fullname">Tipo de inmueble</label>
@@ -282,13 +282,13 @@
                                             </div>
                                         @enderror
                                     </div>
-                                
+
                                     <div class="col-3" id="fecha_alquiler" >
                                         <label class="form-label" for="basic-default-fullname">Fecha vencimiento alquiler</label>
                                         <input value="{{$expediente->detalleInmueble->fecha_venc_alquiler}}" type="date" name="fecha_vencimiento_alquiler" class="form-control" id="fechaVencimiento" />
                                     </div>
                                 </div>
-                            
+
                             {{-- BOTON PARA CARGAR EL PDF DE LA SOLICITUD --}}
                             <div class="col-4 mb-5">
                                 <label class="form-label" for="basic-default-fullname">Solicitud</label>
@@ -312,7 +312,7 @@
                             </div>
                             <div class="col-12 mb-3">
                                 <label class="form-label" for="basic-default-fullname">Observaciones Generales</label>
-                                <textarea cols="40" rows="5" placeholder="Observaciones.." name="observaciones_grales" class="form-control">{{$expediente->observaciones_grales}}</textarea> 
+                                <textarea cols="40" rows="5" placeholder="Observaciones.." name="observaciones_grales" class="form-control">{{$expediente->observaciones_grales}}</textarea>
                             </div>
                             <button type="submit" class="mt-4 btn btn btn-success btn-salir">Siguiente</button>
                         </form>
